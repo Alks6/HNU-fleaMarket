860f7caa2a0fb8566efda91da9473b5b3730b659
Alks6 <60504667+Alks6@users.noreply.github.com>
2021-11-17 18:32:06 +0800

    Update urls.py

--- HNUfleaMarket/HNUfleaMarket/urls.py
+++ HNUfleaMarket/HNUfleaMarket/urls.py
@@ -1,4 +1,4 @@
-"""HNUfleaMarket URL Configuration
+"""FlyCold URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from SchoolBuy import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += urls.urlpatterns
